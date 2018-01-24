3318c4bbee184e3a559cd784d5e63b3ba1f50d51
Samuel Vargas <savarga1@asu.edu>
2018-01-23 23:33:51 -0700

    World initalizes cell Map, has functions to check for items

--- World.hx
+++ World.hx
@@ -14,12 +14,45 @@ class World {
 
     public static function init() {
         World.cells = new Map<Point, Entity>();
+
+        /*
+            Initalize all valid onscreen cell locations to
+            null by default. These will be used to detect
+            collisions with apples and snakes colliding with
+            themselves.
+        */
+
+        var y = 0;
+        while (y < Settings.SCREEN_HEIGHT) {
+            var x = 0;
+            while (x < Settings.SCREEN_WIDTH) {
+                World.cells[new Point(x, y)] = null;
+                x += Settings.CELL_WIDTH;
+            }
+            y += Settings.CELL_HEIGHT;
+        }
+
+        for (y in 0 ... Settings.SCREEN_HEIGHT) {
+            for (x in 0 ... Settings.SCREEN_WIDTH) {
+            }
+        }
     }
 
     public static function reset() {
         for (key in World.cells.keys()) {
             World.cells[key] = null;
         }
+    }
+
+    public static function getAvailableRandomSpawn() {
+        for (spot in World.cells.keys()) {
+            if (World.cells[spot] == null) {
+                return spot;
+            }
+        }
+
+        trace("Entire board is full!");
+        return null;
     }
 
     public static function markOccupied(p: Point, entity: Entity) {
@@ -31,10 +64,22 @@ class World {
     }
 
     public static function isAppleHere(p: Point): Bool {
-        return false;
+        var apple = World.cells[p];
+        if (apple == null) {
+            return false;
+        }
+
+        // TODO: Check if the type is an Apple without reflection
+        var className = Type.getClassName(Apple);
+        var objName = Type.getClassName(Type.getClass(apple));
+        return className == objName;
     }
 
-    public static function isSnakeHere(p: Point): Bool {
-        return false;
+    public static function isSnakeHere(p: Point, entity: Entity): Bool {
+        var snake = World.cells[p];
+        if (snake == null) {
+            return false;
+        }
+        return snake == entity;
     }
 }
