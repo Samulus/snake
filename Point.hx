7b78e0f2dc0eee40cc919ca28194408d98e5fb6b
Samuel Vargas <savarga1@asu.edu>
2018-01-23 18:23:15 -0700

    Add new Point module

---
+++ Point.hx
@@ -0,0 +1,16 @@
+/*
+    Point.hx
+    Author: Samuel Vargas
+*/
+
+package ;
+
+class Point {
+    public var x: Int;
+    public var y: Int;
+
+    public function new(x: Int, y: Int) {
+        this.x = x;
+        this.y = y;
+    }
+}
