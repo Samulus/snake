917c5dccde48ed3d8c29455dcf202dc8dfd139f5
Samuel Vargas <savarga1@asu.edu>
2018-01-23 18:23:34 -0700

    Add ability for single Snake player to move around

--- Player.hx
+++ Player.hx
@@ -10,32 +10,62 @@ import flash.display.Shape;
 
 class Player {
 
-    private var x: Int;
-    private var y: Int;
+    private var position: Point;
+    private var velocity: Point;
     private var w: Int;
     private var h: Int;
     private var shape: Shape;
     private var stage: Stage;
     private var direction: Direction;
 
-    public function new(x, y, w, h, stage) {
-        this.x = x;
-        this.y = y;
+    public function new(x: Int, y: Int, w: Int, h: Int, stage: Stage) {
+        this.position = new Point(x, y);
         this.w = w;
         this.h = h;
         this.shape = new Shape();
         this.stage = stage;
+
+        // Set Direction + Velocity
+        this.direction = Direction.South;
+        this.velocity = new Point(0, 1);
     }
 
-    public function move() {
+    public function move(desired: Direction) {
+        if (desired == Direction.North && direction != Direction.South) {
+            this.velocity.x = 0;
+            this.velocity.y = -1;
+            this.direction = desired;
+        }
+
+        else if (desired == Direction.South && direction != Direction.North) {
+            this.velocity.x = 0;
+            this.velocity.y = 1;
+            this.direction = desired;
+        }
+
+        else if (desired == Direction.East && direction != Direction.West) {
+            this.velocity.x = -1;
+            this.velocity.y = 0;
+            this.direction = desired;
+        }
+
+        else if (desired == Direction.West && direction != Direction.East) {
+            this.velocity.x = 1;
+            this.velocity.y = 0;
+            this.direction = desired;
+        }
     }
 
     public function update() {
+        this.position.x += this.velocity.x;
+        this.position.y += this.velocity.y;
     }
 
     public function render() {
         shape.graphics.beginFill(0x333333);
-        shape.graphics.drawRect(this.x, this.y, this.w, this.h);
+        shape.graphics.drawRect(0, 0, this.w, this.h);
+        shape.x = this.position.x;
+        shape.y = this.position.y;
         stage.addChild(shape);
     }
 }
--- Snake.hx
+++ Snake.hx
@@ -3,15 +3,39 @@
     Author: Samuel Vargas
 */
 
+package ;
+
 import flash.Lib;
 import flash.events.Event;
+import flash.ui.Keyboard;
 
 class Snake {
+    static private var player: Player;
+
     static public function main() {
+        /* Setup Player */
+        player = new Player(32, 32, 16, 16, Lib.current.stage);
+
+        /* Setup Systems */
         Input.init(Lib.current.stage);
+
+        /* Start Loop */
         Lib.current.stage.addEventListener(flash.events.Event.ENTER_FRAME, loop);
     }
 
     static public function loop(event: Event):Void {
+
+        if (Input.isPressed(Keyboard.S)) {
+            player.move(Direction.South);
+        } else if (Input.isPressed(Keyboard.W)) {
+            player.move(Direction.North);
+        } else if (Input.isPressed(Keyboard.A)) {
+            player.move(Direction.East);
+        } else if (Input.isPressed(Keyboard.D)) {
+            player.move(Direction.West);
+        }
+
+        player.update();
+        player.render();
     }
 }
