7f35f7cd15c8ce374c39e57eff0e90623e7d5511
Samuel Vargas <savarga1@asu.edu>
2018-01-23 17:59:32 -0700

    Add onEnterFrame based loop

--- Snake.hx
+++ Snake.hx
@@ -1,5 +1,17 @@
+/*
+    Snake.hx
+    Author: Samuel Vargas
+*/
+
+import flash.Lib;
+import flash.events.Event;
+
 class Snake {
     static public function main() {
-        trace("Snake");
+        Input.init(Lib.current.stage);
+        Lib.current.stage.addEventListener(flash.events.Event.ENTER_FRAME, loop);
+    }
+
+    static public function loop(event: Event):Void {
     }
 }
