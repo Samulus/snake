fdb79dd936ee30bb873b4028cd6110b8e5327f4f
Samuel Vargas <savarga1@asu.edu>
2018-01-24 10:42:33 -0700

    Modify cell size in settings

--- Settings.hx
+++ Settings.hx
@@ -6,8 +6,8 @@
 package ;
 
 class Settings {
-    public static var CELL_HEIGHT = 8;
-    public static var CELL_WIDTH = 8;
+    public static var CELL_HEIGHT = 16; // Fails on 24
+    public static var CELL_WIDTH = 16;
     public static var SCREEN_WIDTH = 400;
     public static var SCREEN_HEIGHT = 300;
     public static var MS_PER_UPDATE = 100;
