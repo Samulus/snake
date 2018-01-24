c6c95dddeadb1e41f21620b6c325a5376e7fc403
Samuel Vargas <savarga1@asu.edu>
2018-01-24 10:07:48 -0700

    Fix implementation of Fisher-Yates

--- Shuffle.hx
+++ Shuffle.hx
@@ -6,17 +6,14 @@
 package ;
 
 class Shuffle {
-
     public static function fisherYates(arr: Array<Point>) {
-        var i = arr.length;
+        var i = arr.length - 1;
         while (i > 0) {
+            var j = Std.random(i + 1);
             var tmp = arr[i];
-            i = i - 1;
-            var random = Std.random(i);
-            arr[i] = arr[random];
-            arr[random] = tmp;
+            arr[i] = arr[j];
+            arr[j] = tmp;
+            --i;
         }
-
     }
-
 }
