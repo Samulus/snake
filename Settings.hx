6ae67303f58d7a6ce9c5ac8eb29f0b08a0aabc6f
Samuel Vargas <savarga1@asu.edu>
2018-01-23 23:35:27 -0700

    Change Snake to Cell, Add Screen Size

--- Settings.hx
+++ Settings.hx
@@ -6,6 +6,8 @@
 package ;
 
 class Settings {
-    public static var SNAKE_HEIGHT = 16;
-    public static var SNAKE_WIDTH = 16;
+    public static var CELL_HEIGHT = 8;
+    public static var CELL_WIDTH = 8;
+    public static var SCREEN_WIDTH = 400;
+    public static var SCREEN_HEIGHT = 300;
 }
