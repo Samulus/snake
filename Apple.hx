847e69c9538534ed76e8e9f7e804ef8f3fb579b6
Samuel Vargas <savarga1@asu.edu>
2018-01-23 23:34:40 -0700

    Add basic code to spawn and render an Apple

--- Apple.hx
+++ Apple.hx
@@ -8,16 +8,24 @@
 */
 
 package ;
+import flash.Lib;
+import flash.display.Stage;
+import flash.display.Shape;
 
-class Apple {
+class Apple implements Entity {
 
-    private static var applePresent = false;
+    private static var position: Point;
+    private static var shape: Shape;
 
-    public static function init() {
-        applePresent = false;
+    public static function spawn(point: Point) {
+        position = Point.clone(point);
+        shape = new Shape();
+        Lib.current.stage.addChild(shape);
     }
 
-    public static function spawn(players: Array<Player>) {
+    public static function render() {
+        shape.graphics.beginFill(0xFF0000);
+        shape.graphics.drawRect(0, 0, Settings.CELL_WIDTH, Settings.CELL_HEIGHT);
     }
 
 }
