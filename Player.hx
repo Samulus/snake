e34513d6c5e467547b51efedc562a03618b67f66
Samuel Vargas <savarga1@asu.edu>
2018-01-23 20:59:32 -0700

    Player now uses arrays / maps to render each segment of the snake

--- Player.hx
+++ Player.hx
@@ -10,27 +10,46 @@ import flash.display.Shape;
 
 class Player {
 
-    private var position: Point;
+    private var positions: Array<Point>;
+    private var collisionMap: Map<Point, Bool>; // I would use a Set<Point> if Haxe had it
+    private var lastTail: Point;
+    private var blocks: Array<Shape>;
     private var velocity: Point;
     private var w: Int;
     private var h: Int;
-    private var shape: Shape;
-    private var stage: Stage;
     private var direction: Direction;
+    private var stage: Stage;
+    private var dead: Bool;
 
     public function new(x: Int, y: Int, w: Int, h: Int, stage: Stage) {
-        this.position = new Point(x, y);
+        // Setup positioning information
+        this.positions = new Array<Point>();
+        this.positions.insert(0, new Point(x, y));
+        this.collisionMap = new Map<Point, Bool>();
+        this.lastTail = new Point(x, y);
         this.w = w;
         this.h = h;
-        this.shape = new Shape();
         this.stage = stage;
+
+        // Setup actual blocks to be rendered
+        this.blocks = new Array<Shape>();
+        this.blocks.insert(0, new Shape());
+        this.stage.addChild(this.blocks[0]);
 
         // Set Direction + Velocity
         this.direction = Direction.South;
         this.velocity = new Point(0, 1);
+
+        // Set life
+        this.dead = false;
     }
 
     public function move(desired: Direction) {
+
+        if (dead) {
+            return;
+        }
+
         if (desired == Direction.North && direction != Direction.South) {
             this.velocity.x = 0;
             this.velocity.y = -1;
@@ -57,15 +76,59 @@ class Player {
     }
 
     public function update() {
-        this.position.x += this.velocity.x;
-        this.position.y += this.velocity.y;
+
+        if (dead) {
+            return;
+        }
+
+        // Record the new free cell that moving the snake always produces
+        // TODO: I don't know if cloning objects is required using Haxe but
+        //       I'm doing it anyway to avoid any nasty reference related
+        //       bugs.
+        lastTail = Point.clone(this.positions[this.positions.length - 1]);
+
+        // Update the body of the snake.
+        for (i in this.positions.length - 1 ... 0) {
+            this.positions[i].x = this.positions[i - 1].x;
+            this.positions[i].y = this.positions[i - 1].y;
+        }
+
+        // Update the head of the snake.
+        this.positions[0].x += this.velocity.x;
+        this.positions[0].y += this.velocity.y;
+
+        // Check if the snake is dead
+        checkIfSnakeDead();
+    }
+
+    private function checkIfSnakeDead() {
+        /* TODO:
+            1) Snake has collided with themselves (use collisionMap)
+            2) Snake has hit the wall.
+        */
     }
 
     public function render() {
-        shape.graphics.beginFill(0x333333);
-        shape.graphics.drawRect(0, 0, this.w, this.h);
-        shape.x = this.position.x;
-        shape.y = this.position.y;
-        stage.addChild(shape);
+        if (dead) {
+            return;
+        }
+
+        for (i in 0 ... this.blocks.length) {
+            this.blocks[i].graphics.beginFill(0x333333);
+            this.blocks[i].graphics.drawRect(0, 0, this.w, this.h);
+            this.blocks[i].x = this.positions[i].x;
+            this.blocks[i].y = this.positions[i].y;
+        }
+    }
+
+    public function growSnake(p: Point) {
+    }
+
+    public function getHead(): Point {
+        return this.positions[0];
+    }
+
+    public function getLastTail(): Point {
+        return this.lastTail;
     }
 }
