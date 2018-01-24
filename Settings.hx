f31c67945cdaf9068ef034925c9905a885aa0c27
Samuel Vargas <savarga1@asu.edu>
2018-01-24 16:23:27 -0700

    Update comment about game only working for 16px

--- Settings.hx
+++ Settings.hx
@@ -6,7 +6,7 @@
 package ;
 
 class Settings {
-    public static var CELL_HEIGHT = 16; // Fails on 24
+    public static var CELL_HEIGHT = 16; // Only works for 16?
     public static var CELL_WIDTH = 16;
     public static var SCREEN_WIDTH = 400;
     public static var SCREEN_HEIGHT = 300;
